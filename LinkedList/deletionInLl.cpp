cec4505c5aa0ee32f5dedc0d30a901e4e0dc3465
souvik <41781438+Souvikns@users.noreply.github.com>
2019-10-12 14:58:38 +0530

    as

--- LinkedList/deletionInLl.cpp
+++ LinkedList/deletionInLl.cpp
@@ -4,20 +4,25 @@ using namespace std;
 struct Node{
   int data;
   Node* next=NULL;
+  Node* prev=NULL;
 };
 class list{
   Node* head=NULL;
   Node* tail=NULL;
 public:
   void insert(int data){
-    Node* node=new Node;
+    Node* node= new Node;
     node->data=data;
     if(head==NULL){
+      node->prev=NULL;
       head=node;
-      tail=node;
     }else{
-      tail->next=node;
-      tail=tail->next;
+      Node* n=head;
+      while(n->next!=NULL){
+        n=n->next;
+      }
+      node->prev=node;
+      n->next=node;
     }
   }
   void show(){
