88272ca90f548d922cd5adec394fc9a04ebe1f44
souvik <41781438+Souvikns@users.noreply.github.com>
2019-10-26 18:17:32 +0530

    concat linked lists

---
+++ LinkedList/concat.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+  int data;
+  Node* next=NULL;
+};
+
+class list{
+public:
+    Node* head=NULL;
+  void insert(int data){
+    Node* node= new Node;
+    node->data=data;
+    if(head==NULL){
+      head=node;
+    }else{
+      Node* n=head;
+      while(n->next!=NULL){
+        n=n->next;
+      }
+      n->next=node;
+    }
+  }
+  void show(){
+    Node* n=head;
+    while(n->next!=NULL){
+      cout<<n->data<<" ";
+      n=n->next;
+    }
+    cout<<n->data<<endl;
+  }
+};
+
+void concat(list a,list b){
+  Node* n=a.head;
+  while(n->next!=NULL){
+    n=n->next;
+  }
+  n->next=b.head;
+}
+
+int main(){
+  list x;
+  for(int i=0;i<5;i++){
+    x.insert(i+1);
+  }
+  x.show();
+  list y;
+  for(int i=0;i<5;i++){
+    y.insert(i+1);
+  }
+  y.show();
+  concat(x,y);
+  x.show();
+}
