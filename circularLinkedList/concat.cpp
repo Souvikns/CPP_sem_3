c65a26755946b070c8e27e25339c53ace29df72c
souvik <41781438+Souvikns@users.noreply.github.com>
2019-10-26 18:32:36 +0530

    init

---
+++ circularLinkedList/concat.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+  int data;
+  Node* next=NULL;
+};
+
+class list{
+public:
+  Node* last=NULL;
+  void insert(int data){
+    Node* node = new Node;
+    node->data=data;
+    if(last==NULL){
+      last=node;
+      last->next=last;
+    }else{
+      node->next=last->next;
+      last->next=node;
+      last=node;
+    }
+  }
+  void show(){
+    Node* n=last->next;
+    if(last==NULL){
+      cout<<"list is empty"<<endl;
+      return;
+    }
+    do {
+      cout<<n->data<<" ";
+      n=n->next;
+    } while(n!=last->next);
+    cout<<endl;
+  }
+};
+
+
+int main(){
+  list x;
+  for(int i=0;i<5;i++){
+    x.insert(i+1);
+  }
+  x.show();
+
+}
