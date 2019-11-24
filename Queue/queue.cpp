b9d71a7034f27e3225cd7a75cdfba7d6377cc2b8
souvik <41781438+Souvikns@users.noreply.github.com>
2019-11-25 00:44:53 +0530

    completed queue

--- Queue/queue.cpp
+++ Queue/queue.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 class Queue{
+public:
   int front,rear,size;
   int capacity;
   int* arr;
@@ -20,9 +21,38 @@ bool isEmpty(Queue* queue){
 }
 
 bool isFull(Queue* queue){
-  
+  return (queue->size==queue->capacity);
 }
 
-int main(){
+void enqueue(Queue* queue,int item){
+  if(isFull(queue)){
+    cout<<"Overflow"<<endl;
+    return;
+  }
+  queue->rear++;
+  queue->arr[queue->rear]=item;
+  queue->size++;
+}
 
+int dequeue(Queue* queue){
+  if(isEmpty(queue)){
+    cout<<"Underflow"<<endl;
+    return INT_MIN;
+  }
+  int item=queue->arr[queue->front];
+  queue->front++;
+  queue->size--;
+  return item;
+}
+
+
+int main(){
+  Queue* x=create(5);
+  enqueue(x,1);
+  enqueue(x,2);
+  enqueue(x,3);
+  enqueue(x,4);
+
+  cout<<dequeue(x)<<endl;
+  cout<<dequeue(x)<<endl;
 }
