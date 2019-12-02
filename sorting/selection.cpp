ed3c02ea7d3e20da870e186ca5624a12e2e5381e
souvik <41781438+Souvikns@users.noreply.github.com>
2019-12-03 02:13:58 +0530

    fixed selection sort

--- sorting/selection.cpp
+++ sorting/selection.cpp
@@ -12,10 +12,11 @@ int main(){
   int arr[]={5,4,3,2,1};
   int l = sizeof(arr)/sizeof(arr[0]);
 
-  int smallest=arr[0];
-  int index=0;
+
 
   for(int i=0;i<l-1;i++){
+    int smallest=arr[i];
+    int index=i;
     for(int j=i;j<l;j++){
       if(smallest>arr[j]){
         smallest=arr[j];
